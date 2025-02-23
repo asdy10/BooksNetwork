5b1461d5bcbcb413e43c158780f9639de3d66203
asdy10 <asdyasdfghjkl@yandex.ru>
2025-02-23 20:07:05 +0500

    d

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ app.include_router(book_router, prefix="/book")
 app.include_router(user_router, prefix="/user")
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)#95.163.85.129
