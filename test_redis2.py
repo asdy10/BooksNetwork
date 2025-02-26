78d9e10fb42d493bd6aae078868cd46b1f3955df
asdy10 <asdyasdfghjkl@yandex.ru>
2025-02-26 20:44:42 +0500

    d

--- test_redis2.py
+++ test_redis2.py
@@ -1,11 +1,13 @@
-
 import os
 import json
 import time
 import random
 import redis
 from datetime import datetime
+from dotenv import load_dotenv
 
+# Загружаем переменные из .env
+load_dotenv()
 REDIS_USER = os.getenv("REDIS_USER")
 REDIS_USER_PASSWORD = os.getenv("REDIS_USER_PASSWORD")
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
@@ -15,7 +17,6 @@ INTERVAL = int(os.getenv('WORKER_INTERVAL', 10))  # 60 seconds by default
 
 
 def redis_operations():
-    print('start redis_operations')
     r = redis.Redis(
         host=REDIS_HOST,
         port=REDIS_PORT,
@@ -24,39 +25,39 @@ def redis_operations():
         password=REDIS_USER_PASSWORD,
         decode_responses=True
     )
-    print('connection success')
 
     while True:
-        try:
-            # Чтение специального ключа
-            special_data = r.get("special_key")
-            print(f"Special key value: {special_data}")
+        # try:
+        # Чтение специального ключа
+        t = time.time()
+        special_data = r.get("special_key")
+        print(f"get", time.time() - t)
+        t = time.time()
+        # Обновление специального ключа
+        new_data = {
+            "timestamp": datetime.now().isoformat(),
+            "value": random.randint(0, 1000)
+        }
+        r.set("special_key", json.dumps(new_data))
+        print('set', time.time() - t)
+        t = time.time()
+        # Чтение 10 случайных ключей
+        for _ in range(10):
+            key = f"key_{random.randint(0, 9999)}"
+            data = r.get(key)
+            print(f"Read {key}: {data[:30]}..." if data else f"{key} not found")
 
-            # Обновление специального ключа
-            new_data = {
-                "timestamp": datetime.now().isoformat(),
-                "value": random.randint(0, 1000)
-            }
-            r.set("special_key", json.dumps(new_data))
+        print('set many', time.time() - t)
+        time.sleep(INTERVAL)
 
-            # Чтение 10 случайных ключей
-            for _ in range(10):
-                key = f"key_{random.randint(0, 9999)}"
-                data = r.get(key)
-                print(f"Read {key}: {data[:30]}..." if data else f"{key} not found")
-
-            print(f"Operations completed. Sleeping for {INTERVAL} seconds")
-            time.sleep(INTERVAL)
-
-        except redis.RedisError as e:
-            print(f"Redis error: {e}")
-            time.sleep(5)
-        except Exception as e:
-            print(f"General error: {e}")
-            time.sleep(5)
+        # except redis.RedisError as e:
+        #     print(f"Redis error: {e}")
+        #     time.sleep(5)
+        # except Exception as e:
+        #     print(f"General error: {e}")
+        #     time.sleep(5)
 
 
-print('init')
 if __name__ == "__main__":
     print('start')
     redis_operations()
