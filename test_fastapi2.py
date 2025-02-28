1aba5992ac4c9c16fed5ccafc23101af0ecdfb3a
asdy10 <asdyasdfghjkl@yandex.ru>
2025-02-28 21:57:11 +0500

    d

--- test_fastapi2.py
+++ test_fastapi2.py
@@ -7,7 +7,7 @@ while True:
     t = time.time()
     # Чтение 10 случайных ключей
     for _ in range(100):
-        res = requests.get('api-worker:8000/get')
+        res = requests.get('http://api-worker:8000/get')
 
     print('get many', time.time() - t)
     time.sleep(10)
